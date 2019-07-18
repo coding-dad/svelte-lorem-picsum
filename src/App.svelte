7852f10be7f5bdf39fac6f3d08e6b6ff5b6f8d68
coding-dad <boris.365@outlook.com>
2019-07-18 23:30:43 +0200

    working version

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,84 @@
 <script>
   import ImageView from "./components/ImageView.svelte";
+  import { onMount } from "svelte";
 
-  const range = (start, end) => {
-    return Array(end - start + 1)
-      .fill()
-      .map((_, idx) => start + idx);
+  export let page = 1;
+  export let limit = 6;
+
+  let overlayVisible = false;
+  let imageList = [];
+  let overlayImageUrl = "";
+
+  onMount(() => {
+    loadImageList();
+  });
+
+  const loadImageList = async () => {
+    const req = await fetch(
+      `https://picsum.photos/v2/list?page=${page}&limit=${limit}`
+    );
+
+    const json = await req.json();
+    imageList = json;
   };
+
+  const onClickNext = async () => {
+    page++;
+    loadImageList();
+  };
+
+  const onClickBack = async () => {
+    if (page === 1) return;
+
+    page--;
+    loadImageList();
+  };
+
+  const onImageClicked = event => {
+    overlayImageUrl = event.detail.imageSource;
+    overlayVisible = true;
+  };
+
+  $: overlayStyle = overlayVisible
+    ? `background-image: url("${overlayImageUrl}");`
+    : "";
 </script>
 
 <style>
   h1 {
     color: rgb(32, 32, 32);
+  }
+
+  .overlay {
+    width: 80%;
+    height: 80%;
+
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    color: #a1a1a1;
+
+    border-radius: 0.25rem;
+    padding: 1rem;
+
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+
+    box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1),
+      0 4px 6px -2px rgba(0, 0, 0, 0.05);
+  }
+
+  .overlay::before {
+    content: "\00d7";
+    display: block;
+    width: 100%;
+    text-align: right;
   }
 
   .images {
@@ -21,6 +89,7 @@
 
   .container {
     display: flex;
+    flex-direction: column;
     width: 80%;
     max-width: 1024px;
     justify-content: center;
@@ -36,16 +105,53 @@
     justify-content: center;
     margin-bottom: 2rem;
   }
+
+  .buttons {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding-left: 2rem;
+    padding-right: 2rem;
+  }
+
+  .buttons button,
+  div {
+    font-size: 1.2rem;
+  }
 </style>
 
-<div class="header">
-  <h1>Svelte Example Lorem Picsum</h1>
-</div>
-
-<div class="container">
-  <div class="images">
-    {#each range(0, 11) as idx}
-      <ImageView width="300" height="200" index={idx} />
-    {/each}
+<div class="content">
+  <div class="header">
+    <h1>Svelte Example Lorem Picsum</h1>
   </div>
+
+  <div class="container">
+    <div class="images">
+      {#each imageList as imageItem}
+        <ImageView
+          on:showImage={onImageClicked}
+          width="300"
+          height="200"
+          imageId={imageItem.id}
+          alt={imageItem.author}
+          src={imageItem.download_url} />
+      {/each}
+    </div>
+    <div class="buttons">
+      {#if page > 1}
+        <button on:click={onClickBack}>&lsaquo;&lsaquo; {page - 1}</button>
+      {:else}
+        <div />
+      {/if}
+      <div>{page}</div>
+      <button on:click={onClickNext}>{page + 1} &rsaquo;&rsaquo;</button>
+    </div>
+  </div>
+
+  {#if overlayVisible}
+    <div
+      class="overlay"
+      style={overlayStyle}
+      on:click={evt => (overlayVisible = false)} />
+  {/if}
 </div>
--- src/components/ImageView.svelte
+++ src/components/ImageView.svelte
@@ -1,15 +1,14 @@
 <script>
+  import { createEventDispatcher } from "svelte";
+
   export let width = 300;
   export let height = width;
-  export let index = 0;
+  export let imageId = 0;
+  export let alt = "an image";
+  export let src = "";
 
+  const dispatch = createEventDispatcher();
   let image = "";
-
-  import { onMount } from "svelte";
-
-  onMount(() => {
-    console.log("the component has mounted", width, height);
-  });
 </script>
 
 <style>
@@ -23,9 +22,9 @@
   }
 </style>
 
-<div>
+<div on:click={() => dispatch('showImage', { imageSource: src })}>
   <img
     class="image"
-    src={`https://picsum.photos/${width}/${height}?random=${index}`}
-    alt="" />
+    src={`https://picsum.photos/id/${imageId}/${width}/${height}`}
+    {alt} />
 </div>
